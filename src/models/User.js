419e183969e7c25f78c0ded08597ce71609edd7a
Kevin Wang <rocwang0316@gmail.com>
2018-11-05 14:13:02 -0800

    Converted the candidateId to a String

--- src/models/User.js
+++ src/models/User.js
@@ -5,4 +5,4 @@ exports.fetchOneUser = username => BlockChainHelper.fetchUser(username);
 exports.registerNewUser = user => BlockChainHelper
   .addUser(user.id, user.password, user.name, user.photo);
 
-exports.vote = (userId, candidateId) => BlockChainHelper.vote(candidateId, userId);
+exports.vote = (userId, candidateId) => BlockChainHelper.vote(candidateId.toString(), userId);
