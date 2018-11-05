6de9a4ca12eb8a57a77ceb6e7d6adbd5f69b7d17
Kevin Wang <rocwang0316@gmail.com>
2018-11-04 22:47:01 -0800

    Updated the login controller to the DApp version.

--- __tests__/controllers/Login.js
+++ __tests__/controllers/Login.js
@@ -1,12 +1,12 @@
 import LoginController from '../../src/controllers/Login';
 
 jest.mock('bcryptjs', () => ({ compare: jest.fn().mockReturnValue(Promise.resolve(true)) }));
-jest.mock('../../src/models/User', () => ({ fetchOneUser: jest.fn().mockReturnValue(Promise.resolve([{ password: 'password' }])) }));
+jest.mock('../../src/models/User', () => ({ fetchOneUser: jest.fn().mockReturnValue(Promise.resolve(['id', 'name', 'hashed password', 'photo', { toNumber: jest.fn().mockReturnValue(0) }])) }));
 jest.mock('../../src/utils/Logger', () => ({ error: jest.fn() }));
 jest.mock('../../src/utils/JWTUtil', () => ({ signJWT: jest.fn().mockReturnValue('JWT') }));
 
 describe('Login controller', () => {
-  test('fetchOneUser without database error and passes compare', async () => {
+  test('fetchOneUser with user and passes compare', async () => {
     const { fetchOneUser } = require('../../src/models/User');
     const { error } = require('../../src/utils/Logger');
     const { compare } = require('bcryptjs');
@@ -21,34 +21,16 @@ describe('Login controller', () => {
     expect(fetchOneUser).toHaveBeenLastCalledWith(req.query.username);
     expect(error).not.toHaveBeenCalled();
     expect(compare).toHaveBeenCalledTimes(1);
-    expect(compare).toHaveBeenLastCalledWith(req.query.password, 'password');
+    expect(compare).toHaveBeenLastCalledWith(req.query.password, 'hashed password');
     expect(signJWT).toHaveBeenCalledTimes(1);
-    expect(signJWT).toHaveBeenLastCalledWith({ password: 'password' });
+    expect(signJWT).toHaveBeenLastCalledWith({ id: 'id', name: 'name', password: 'hashed password', photo: 'photo', vote_id: 0 });
     expect(res.json).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenLastCalledWith('JWT');
   });
 
-  test('fetchOneUser with database error', async () => {
+  test('fetchOneUser with user and does not pass compare', async () => {
     const { fetchOneUser } = require('../../src/models/User');
-    fetchOneUser.mockReturnValueOnce(Promise.reject());
-    const { error } = require('../../src/utils/Logger');
-    const { compare } = require('bcryptjs');
-
-    const req = { query: { username: 'username', password: 'password' } };
-    const res = { json: jest.fn() };
-
-    await LoginController(req, res);
-
-    expect(fetchOneUser).toHaveBeenCalledTimes(2);
-    expect(fetchOneUser).toHaveBeenLastCalledWith(req.query.username);
-    expect(error).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenLastCalledWith({ isFail: true });
-  });
-
-  test('fetchOneUser without database error and does not pass compare', async () => {
-    const { fetchOneUser } = require('../../src/models/User');
-    fetchOneUser.mockReturnValueOnce(Promise.resolve([{ password: 'password' }]));
+    // fetchOneUser.mockReturnValueOnce(Promise.resolve([{ password: 'password' }]));
     const { error } = require('../../src/utils/Logger');
     const { compare } = require('bcryptjs');
     compare.mockReturnValueOnce(Promise.resolve(false));
@@ -58,18 +40,18 @@ describe('Login controller', () => {
 
     await LoginController(req, res);
 
-    expect(fetchOneUser).toHaveBeenCalledTimes(3);
+    expect(fetchOneUser).toHaveBeenCalledTimes(2);
     expect(fetchOneUser).toHaveBeenLastCalledWith(req.query.username);
-    expect(error).toHaveBeenCalledTimes(1);
+    expect(error).not.toHaveBeenCalled();
     expect(compare).toHaveBeenCalledTimes(2);
-    expect(compare).toHaveBeenLastCalledWith(req.query.password, 'password');
+    expect(compare).toHaveBeenLastCalledWith(req.query.password, 'hashed password');
     expect(res.json).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenLastCalledWith({ isFail: true });
   });
 
-  test('fetchOneUser without database error and no result', async () => {
+  test('fetchOneUser no result', async () => {
     const { fetchOneUser } = require('../../src/models/User');
-    fetchOneUser.mockReturnValueOnce(Promise.resolve(null));
+    fetchOneUser.mockReturnValueOnce(Promise.resolve(['111']));
     const { error } = require('../../src/utils/Logger');
     const { compare } = require('bcryptjs');
 
@@ -78,8 +60,22 @@ describe('Login controller', () => {
 
     await LoginController(req, res);
 
-    expect(fetchOneUser).toHaveBeenCalledTimes(4);
+    expect(fetchOneUser).toHaveBeenCalledTimes(3);
     expect(fetchOneUser).toHaveBeenLastCalledWith(req.query.username);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenLastCalledWith({ isFail: true });
+  });
+
+  test('fetchOneUser throw error', async () => {
+    const { fetchOneUser } = require('../../src/models/User');
+    fetchOneUser.mockReturnValueOnce(Promise.reject());
+    const { error } = require('../../src/utils/Logger');
+    const req = { query: { username: 'username', password: 'password' } };
+    const res = { json: jest.fn() };
+
+    await LoginController(req, res);
+
+    expect(error).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenLastCalledWith({ isFail: true });
   });
--- src/controllers/Login.js
+++ src/controllers/Login.js
@@ -6,9 +6,15 @@ const { signJWT } = require('../utils/JWTUtil');
 
 // Use the old promise then for the test purpose
 module.exports = (req, res) => fetchOneUser(req.query.username).then(result => {
-  if (!result || result.length === 0) res.json({ isFail: true });
+  if (!result[1]) res.json({ isFail: true }); // result 1 is name. If it is empty, the user does not exist.
   else {
-    const user = result[0];
+    const user = { // The result order is decided by Vote's contractor's return order. See Vote.sol
+      id: result[0],
+      name: result[1],
+      password: result[2],
+      photo: result[3],
+      vote_id: result[4].toNumber(),
+    };
     bcrypt.compare(req.query.password, user.password).then(compareResult => {
       if (compareResult) {
         // JWTUtil's signJWT will remove the password before return to the client side.
